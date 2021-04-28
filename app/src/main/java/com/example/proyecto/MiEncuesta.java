50939cf5fef692838a01c63261e12862e57d9fde
abcastillo1 <acastilloariel@gmail.com>
2021-04-28 12:50:01 -0500

    Hasta la pregunta 7 de la encuesta(Con la validacion de hacer aparecer campos ocultos)

--- app/src/main/java/com/example/proyecto/MiEncuesta.java
+++ app/src/main/java/com/example/proyecto/MiEncuesta.java
@@ -10,9 +10,10 @@ import android.widget.LinearLayout;
 
 public class MiEncuesta extends AppCompatActivity {
 
-    private CheckBox check_otros,check_otros1,check_si,check_si1;
-    private View campo_otros,campo_otros1;
-    private LinearLayout textPregunta11,textPregunta13;
+    private CheckBox check_otros,check_otros1,check_si,check_si1,check_si2,check_si3,check_rio,check_otros2,check_cisterna,check_tanque,check_si4,check_si5;
+    private View campo_otros,campo_otros1,texto_info19;
+    private LinearLayout textPregunta11,textPregunta13,textPregunta15,textPregunta17,textPregunta19,textPregunta20,textPregunta22,textPregunta23,textPregunta24,
+            textPregunta25,textPregunta26,textPregunta27,textPregunta30;
 
 
     @Override
@@ -31,6 +32,35 @@ public class MiEncuesta extends AppCompatActivity {
 
         check_si1 = (CheckBox) findViewById(R.id.check_si1);
         textPregunta13 = findViewById(R.id.textPregunta13);
+
+        check_si2 = (CheckBox) findViewById(R.id.check_si2);
+        textPregunta15 = findViewById(R.id.textPregunta15);
+
+        check_si3 = (CheckBox) findViewById(R.id.check_si3);
+        textPregunta17 = findViewById(R.id.textPregunta17);
+
+        check_rio = (CheckBox) findViewById(R.id.check_rio);
+        textPregunta19 = findViewById(R.id.textPregunta19);
+
+        check_otros2 = (CheckBox) findViewById(R.id.check_otros2);
+        textPregunta20 = findViewById(R.id.textPregunta20);
+
+        check_cisterna = (CheckBox) findViewById(R.id.check_cisterna);
+        textPregunta22 = findViewById(R.id.textPregunta22);
+
+        check_tanque = (CheckBox) findViewById(R.id.check_tanque);
+        textPregunta23 = findViewById(R.id.textPregunta23);
+        textPregunta24 = findViewById(R.id.textPregunta24);
+        texto_info19= findViewById(R.id.texto_info19);
+        textPregunta25= findViewById(R.id.textPregunta25);
+
+        check_si4 = (CheckBox) findViewById(R.id. check_si4);
+        textPregunta26= findViewById(R.id.textPregunta26);
+        textPregunta27= findViewById(R.id.textPregunta27);
+
+        check_si5 = (CheckBox) findViewById(R.id. check_si5);
+        textPregunta30= findViewById(R.id.textPregunta30);
+
 
         //Si da en otros se mostrará una caja de texto
         check_otros.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -61,6 +91,71 @@ public class MiEncuesta extends AppCompatActivity {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 textPregunta13.setVisibility(isChecked ? View.VISIBLE : View.GONE);
+            }
+        });
+
+
+        check_si2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                textPregunta15.setVisibility(isChecked ? View.VISIBLE : View.GONE);
+            }
+        });
+
+        check_si3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                textPregunta17.setVisibility(isChecked ? View.VISIBLE : View.GONE);
+            }
+        });
+
+        check_rio.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                textPregunta19.setVisibility(isChecked ? View.VISIBLE : View.GONE);
+            }
+        });
+
+        check_otros2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                textPregunta20.setVisibility(isChecked ? View.VISIBLE : View.GONE);
+            }
+        });
+
+        check_cisterna.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                textPregunta22.setVisibility(isChecked ? View.VISIBLE : View.GONE);
+                textPregunta24.setVisibility(isChecked ? View.VISIBLE : View.GONE);
+                texto_info19.setVisibility(isChecked ? View.VISIBLE : View.GONE);
+                textPregunta25.setVisibility(isChecked ? View.VISIBLE : View.GONE);
+            }
+        });
+
+        check_tanque.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                textPregunta23.setVisibility(isChecked ? View.VISIBLE : View.GONE);
+                textPregunta24.setVisibility(isChecked ? View.VISIBLE : View.GONE);
+                texto_info19.setVisibility(isChecked ? View.VISIBLE : View.GONE);
+                textPregunta25.setVisibility(isChecked ? View.VISIBLE : View.GONE);
+            }
+        });
+
+        check_si4.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                textPregunta26.setVisibility(isChecked ? View.VISIBLE : View.GONE);
+                textPregunta27.setVisibility(isChecked ? View.VISIBLE : View.GONE);
+            }
+        });
+
+        check_si5.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                textPregunta30.setVisibility(isChecked ? View.VISIBLE : View.GONE);
+
             }
         });
 
