38dd897df474bf96c07ecfd435f15d5744a7307e
abcastillo1 <acastilloariel@gmail.com>
2021-04-27 23:22:29 -0500

    Más de la mitad de las preguntas de la encuesta

--- app/src/main/java/com/example/proyecto/MiEncuesta.java
+++ app/src/main/java/com/example/proyecto/MiEncuesta.java
@@ -6,11 +6,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.LinearLayout;
 
 public class MiEncuesta extends AppCompatActivity {
 
-    private CheckBox check_otros,check_otros1;
+    private CheckBox check_otros,check_otros1,check_si,check_si1;
     private View campo_otros,campo_otros1;
+    private LinearLayout textPregunta11,textPregunta13;
 
 
     @Override
@@ -24,6 +26,13 @@ public class MiEncuesta extends AppCompatActivity {
         check_otros1 = (CheckBox) findViewById(R.id.check_otros1);
         campo_otros1 = findViewById(R.id.campo_otros1);
 
+        check_si = (CheckBox) findViewById(R.id.check_si);
+        textPregunta11 = findViewById(R.id.textPregunta11);
+
+        check_si1 = (CheckBox) findViewById(R.id.check_si1);
+        textPregunta13 = findViewById(R.id.textPregunta13);
+
+        //Si da en otros se mostrará una caja de texto
         check_otros.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -31,6 +40,7 @@ public class MiEncuesta extends AppCompatActivity {
             }
         });
 
+        //Si da en otros se mostrará una caja de texto
         check_otros1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -38,6 +48,21 @@ public class MiEncuesta extends AppCompatActivity {
             }
         });
 
+        //Si da en "si" se mostrará una pregunta más
+        check_si.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                textPregunta11.setVisibility(isChecked ? View.VISIBLE : View.GONE);
+            }
+        });
+
+        //Si da en "si" se mostrará una pregunta más
+        check_si1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                textPregunta13.setVisibility(isChecked ? View.VISIBLE : View.GONE);
+            }
+        });
 
     }
 }
