18b574ca673930c3c12eecca876e00ff68b865c2
abcastillo1 <acastilloariel@gmail.com>
2021-04-28 17:52:02 -0500

    Activity de la encuesta con todas las preguntas(Solo falta darle color y arreglar la tipografia)

--- app/src/main/java/com/example/proyecto/MiEncuesta.java
+++ app/src/main/java/com/example/proyecto/MiEncuesta.java
@@ -10,10 +10,13 @@ import android.widget.LinearLayout;
 
 public class MiEncuesta extends AppCompatActivity {
 
-    private CheckBox check_otros,check_otros1,check_si,check_si1,check_si2,check_si3,check_rio,check_otros2,check_cisterna,check_tanque,check_si4,check_si5;
+    private CheckBox check_otros,check_otros1,check_si,check_si1,check_si2,check_si3,check_rio,check_otros2,check_cisterna,check_tanque,check_si4,check_si5,
+                     check_animales,check_animal_consumo,check_animal_venta, check_riego,check_sembrio_consumo,check_sembrio_venta;
+
     private View campo_otros,campo_otros1,texto_info19;
+
     private LinearLayout textPregunta11,textPregunta13,textPregunta15,textPregunta17,textPregunta19,textPregunta20,textPregunta22,textPregunta23,textPregunta24,
-            textPregunta25,textPregunta26,textPregunta27,textPregunta30;
+                     textPregunta25,textPregunta26,textPregunta27,textPregunta30,textPregunta32,textPregunta33,textPregunta34,textPregunta35,textPregunta36,textPregunta37;
 
 
     @Override
@@ -54,13 +57,30 @@ public class MiEncuesta extends AppCompatActivity {
         texto_info19= findViewById(R.id.texto_info19);
         textPregunta25= findViewById(R.id.textPregunta25);
 
-        check_si4 = (CheckBox) findViewById(R.id. check_si4);
+        check_si4 = (CheckBox) findViewById(R.id.check_si4);
         textPregunta26= findViewById(R.id.textPregunta26);
         textPregunta27= findViewById(R.id.textPregunta27);
 
-        check_si5 = (CheckBox) findViewById(R.id. check_si5);
+        check_si5 = (CheckBox) findViewById(R.id.check_si5);
         textPregunta30= findViewById(R.id.textPregunta30);
 
+        check_animales = (CheckBox) findViewById(R.id.check_animales);
+        textPregunta32= findViewById(R.id.textPregunta32);
+
+        check_animal_consumo = (CheckBox) findViewById(R.id.check_animal_consumo);
+        textPregunta33= findViewById(R.id.textPregunta33);
+
+        check_animal_venta = (CheckBox) findViewById(R.id.check_animal_venta);
+        textPregunta34 = findViewById(R.id.textPregunta34);
+
+        check_riego = (CheckBox) findViewById(R.id.check_riego);
+        textPregunta35 = findViewById(R.id.textPregunta35);
+
+        check_sembrio_consumo= (CheckBox) findViewById(R.id.check_sembrio_consumo);
+        textPregunta36 = findViewById(R.id.textPregunta36);
+
+        check_sembrio_venta= (CheckBox) findViewById(R.id.check_sembrio_venta);
+        textPregunta37 = findViewById(R.id.textPregunta37);
 
         //Si da en otros se mostrará una caja de texto
         check_otros.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -158,6 +178,59 @@ public class MiEncuesta extends AppCompatActivity {
 
             }
         });
+
+        //En el caso de tener animales que consumen agua indicar:
+        check_animales.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                textPregunta32.setVisibility(isChecked ? View.VISIBLE : View.GONE);
+
+            }
+        });
+
+        //animales para consumo
+        check_animal_consumo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                textPregunta33.setVisibility(isChecked ? View.VISIBLE : View.GONE);
+
+            }
+        });
+
+        //animales para venta
+        check_animal_venta.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                textPregunta34.setVisibility(isChecked ? View.VISIBLE : View.GONE);
+
+            }
+        });
+        //En el caso de tener sembrios que consumen agua indicar:
+        check_riego.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                textPregunta35.setVisibility(isChecked ? View.VISIBLE : View.GONE);
+
+            }
+        });
+        //sembrios para consumo
+        check_sembrio_consumo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                textPregunta36.setVisibility(isChecked ? View.VISIBLE : View.GONE);
+
+            }
+        });
+        //sembrios para venta
+        check_sembrio_venta.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                textPregunta37.setVisibility(isChecked ? View.VISIBLE : View.GONE);
+
+            }
+        });
+
+
 
     }
 }
